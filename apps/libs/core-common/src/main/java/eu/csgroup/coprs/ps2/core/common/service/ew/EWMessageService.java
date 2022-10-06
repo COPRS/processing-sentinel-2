67419960c4e170d02d7982f25d0be5d012421ca9
Patrice FABRE <patrice.fabre@csgroup.eu>
2022-10-06 11:39:38 +0200

    feat(546): add timeliness for S2 products

--- apps/libs/core-common/src/main/java/eu/csgroup/coprs/ps2/core/common/service/ew/EWMessageService.java
+++ apps/libs/core-common/src/main/java/eu/csgroup/coprs/ps2/core/common/service/ew/EWMessageService.java
@@ -4,6 +4,7 @@ import eu.csgroup.coprs.ps2.core.common.model.ExecutionInput;
 import eu.csgroup.coprs.ps2.core.common.model.FileInfo;
 import eu.csgroup.coprs.ps2.core.common.model.processing.ProcessingMessage;
 import eu.csgroup.coprs.ps2.core.common.model.processing.ProductFamily;
+import eu.csgroup.coprs.ps2.core.common.model.processing.Timeliness;
 import eu.csgroup.coprs.ps2.core.common.settings.MessageParameters;
 import eu.csgroup.coprs.ps2.core.common.utils.ProcessingMessageUtils;
 
@@ -27,7 +28,8 @@ public abstract class EWMessageService<T extends ExecutionInput> {
                             .setProductFamily(productFamily)
                             .setStoragePath(fileInfo.getObsURL())
                             .setKeyObjectStorage(fileInfo.getObsName())
-                            .setSatelliteId(executionInput.getSatellite());
+                            .setSatelliteId(executionInput.getSatellite())
+                            .setTimeliness(getTimeliness(productFamily));
 
                     processingMessage.getAdditionalFields().put(MessageParameters.T0_PDGS_DATE_FIELD, executionInput.getT0PdgsDate());
 
@@ -36,6 +38,16 @@ public abstract class EWMessageService<T extends ExecutionInput> {
         );
 
         return messages;
+    }
+
+    private Timeliness getTimeliness(ProductFamily productFamily) {
+        return switch (productFamily) {
+            case S2_SAD, S2_HKTM -> Timeliness.S2_SESSION;
+            case S2_L0_DS, S2_L0_GR -> Timeliness.S2_L0;
+            case S2_L1A_DS, S2_L1A_GR, S2_L1B_DS, S2_L1B_GR, S2_L1C_DS, S2_L1C_TC, S2_L1C_TL -> Timeliness.S2_L1;
+            case S2_L2A_DS, S2_L2A_TL -> Timeliness.S2_L2;
+            default -> Timeliness.EMPTY;
+        };
     }
 
 }

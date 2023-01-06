47718d84776d48879b0047b341d7a3a5696b8a5b
Patrice FABRE <patrice.fabre@csgroup.eu>
2023-01-06 10:48:47 +0100

    refactor: rework pending processes

--- apps/libs/core-pw/src/main/java/eu/csgroup/coprs/ps2/core/pw/service/PWPendingProcessingService.java
+++ apps/libs/core-pw/src/main/java/eu/csgroup/coprs/ps2/core/pw/service/PWPendingProcessingService.java
@@ -3,31 +3,50 @@ package eu.csgroup.coprs.ps2.core.pw.service;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.function.Supplier;
 
 @Slf4j
-public abstract class PWPendingProcessingService {
+@Service
+@ConditionalOnBean(PWItemService.class)
+public class PWPendingProcessingService {
 
+    @Value("${pending.gaugeName}")
+    private String gaugeName;
+
+    @Value("${pending.mission}")
+    private String mission;
+
+    @Value("${pending.level}")
+    private String level;
+
+    @Value("${pending.addon}")
+    private String addon;
+
+    private final MeterRegistry registry;
     private final PWItemService<?, ?> itemService;
 
-    protected PWPendingProcessingService(MeterRegistry registry, PWItemService<?, ?> itemService) {
-
+    public PWPendingProcessingService(MeterRegistry registry, PWItemService<?, ?> itemService) {
+        this.registry = registry;
         this.itemService = itemService;
+    }
 
-        log.info("Setting up metric: Pending processing Gauge - Mission: S2 - Level: {} - Addon: {}", getLevel(), getAddonName());
+    @PostConstruct
+    public void init() {
 
-        Gauge.builder("rs.pending.processing.job", fetchPendingSessions())
-                .tag("mission", "S2")
-                .tag("level", getLevel())
-                .tag("addonName", getAddonName())
+        log.info("Setting up metric: Pending processing Gauge - Mission: {} - Level: {} - Addon: {}", mission, level, addon);
+
+        Gauge.builder(gaugeName, fetchPendingSessions())
+                .tag("mission", mission)
+                .tag("level", level)
+                .tag("addonName", addon)
                 .description("Show the number of pending processing entries")
                 .register(registry);
     }
-
-    protected abstract String getLevel();
-
-    protected abstract String getAddonName();
 
     private Supplier<Number> fetchPendingSessions() {
         return () -> itemService.readAll(false, false).size();
